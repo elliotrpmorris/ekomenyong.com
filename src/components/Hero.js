6e6fdc35a0d35e21bfaa9ac1732599db17ec9222
Ekom Enyong <hello@ekomenyong.com>
2021-03-10 02:41:21 -0500

    🏗️  Updated Hero spacing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Header({ title, subTitle }) {
   return (
-    <div className="max-w-4xl mx-auto my-12 px-4 md:px-0">
+    <div className="max-w-4xl mx-auto my-12">
       <div className="">
         <h1 className="text-4xl md:text-5xl font-bold leading-tight tracking-tight mb-4">
           {title}
